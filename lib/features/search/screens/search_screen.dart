810a2390faec8859d2cac26c03e60abd60dd9257
Shreyas Shirwadkar <shreyas.shirwadkar1@gmail.com>
2025-04-25 09:26:22 +0530

    Created Search Screen

--- lib/features/search/screens/search_screen.dart
+++ lib/features/search/screens/search_screen.dart
@@ -1,25 +1,49 @@
+import 'package:ecom/common/widgets/loader.dart';
 import 'package:ecom/constants/global_variables.dart';
 import 'package:ecom/features/home/widgets/address_box.dart';
-import 'package:ecom/features/home/widgets/carousel_image.dart';
-import 'package:ecom/features/home/widgets/deal_of_day.dart';
-import 'package:ecom/features/home/widgets/top_categories.dart';
-import 'package:ecom/features/search/screens/search_screen.dart';
+import 'package:ecom/features/product_details/screens/product_details_screen.dart';
+import 'package:ecom/features/search/services/search_services.dart';
+import 'package:ecom/features/search/widget/searched_product.dart';
+import 'package:ecom/models/product.dart';
 import 'package:flutter/material.dart';
 
-class HomeScreen
+class SearchScreen
     extends StatefulWidget {
   static const String routeName =
-      '/home';
-  const HomeScreen({Key? key})
-    : super(key: key);
+      '/search-screen';
+  final String searchQuery;
+  const SearchScreen({
+    Key? key,
+    required this.searchQuery,
+  }) : super(key: key);
 
   @override
-  State<HomeScreen> createState() =>
-      _HomeScreenState();
+  State<SearchScreen> createState() =>
+      _SearchScreenState();
 }
 
-class _HomeScreenState
-    extends State<HomeScreen> {
+class _SearchScreenState
+    extends State<SearchScreen> {
+  List<Product>? products;
+  final SearchServices searchServices =
+      SearchServices();
+
+  @override
+  void initState() {
+    super.initState();
+    fetchSearchedProduct();
+  }
+
+  fetchSearchedProduct() async {
+    products = await searchServices
+        .fetchSearchedProduct(
+          context: context,
+          searchQuery:
+              widget.searchQuery,
+        );
+    setState(() {});
+  }
+
   void navigateToSearchScreen(
     String query,
   ) {
@@ -150,18 +174,44 @@ class _HomeScreenState
           ),
         ),
       ),
-      body: SingleChildScrollView(
-        child: Column(
-          children: const [
-            AddressBox(),
-            SizedBox(height: 10),
-            TopCategories(),
-            SizedBox(height: 10),
-            CarouselImage(),
-            DealOfDay(),
-          ],
-        ),
-      ),
+      body:
+          products == null
+              ? const Loader()
+              : Column(
+                children: [
+                  const AddressBox(),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  Expanded(
+                    child: ListView.builder(
+                      itemCount:
+                          products!
+                              .length,
+                      itemBuilder: (
+                        context,
+                        index,
+                      ) {
+                        return GestureDetector(
+                          // onTap: () {
+                          //   Navigator.pushNamed(
+                          //     context,
+                          //     ProductDetailScreen
+                          //         .routeName,
+                          //     arguments:
+                          //         products![index],
+                          //   );
+                          // },
+                          child: SearchedProduct(
+                            product:
+                                products![index],
+                          ),
+                        );
+                      },
+                    ),
+                  ),
+                ],
+              ),
     );
   }
 }
--- lib/router.dart
+++ lib/router.dart
@@ -1,8 +1,14 @@
 import 'package:ecom/common/widgets/bottom_bar.dart';
+import 'package:ecom/features/address/screens/address_screen.dart';
 import 'package:ecom/features/admin/screens/add_product_screen.dart';
 import 'package:ecom/features/auth/screens/auth_screen.dart';
 import 'package:ecom/features/home/screens/category_deals_screen.dart';
 import 'package:ecom/features/home/screens/home_screen.dart';
+import 'package:ecom/features/order_details/screens/order_details.dart';
+import 'package:ecom/features/product_details/screens/product_details_screen.dart';
+import 'package:ecom/features/search/screens/search_screen.dart';
+import 'package:ecom/models/order.dart';
+import 'package:ecom/models/product.dart';
 import 'package:flutter/material.dart';
 
 Route<dynamic> generateRoute(
@@ -11,43 +17,94 @@ Route<dynamic> generateRoute(
   switch (routeSettings.name) {
     case AuthScreen.routeName:
       return MaterialPageRoute(
+        settings: routeSettings,
         builder:
             (_) => const AuthScreen(),
       );
+
     case HomeScreen.routeName:
       return MaterialPageRoute(
+        settings: routeSettings,
         builder:
             (_) => const HomeScreen(),
       );
     case BottomBar.routeName:
       return MaterialPageRoute(
+        settings: routeSettings,
         builder:
             (_) => const BottomBar(),
       );
     case AddProductScreen.routeName:
       return MaterialPageRoute(
+        settings: routeSettings,
         builder:
             (_) =>
                 const AddProductScreen(),
       );
+
     case CategoryDealsScreen.routeName:
       var category =
           routeSettings.arguments
               as String;
       return MaterialPageRoute(
+        settings: routeSettings,
         builder:
             (_) => CategoryDealsScreen(
               category: category,
             ),
       );
-
+    case SearchScreen.routeName:
+      var searchQuery =
+          routeSettings.arguments
+              as String;
+      return MaterialPageRoute(
+        settings: routeSettings,
+        builder:
+            (_) => SearchScreen(
+              searchQuery: searchQuery,
+            ),
+      );
+    // case ProductDetailScreen.routeName:
+    //   var product =
+    //       routeSettings.arguments
+    //           as Product;
+    //   return MaterialPageRoute(
+    //     settings: routeSettings,
+    //     builder:
+    //         (_) => ProductDetailScreen(
+    //           product: product,
+    //         ),
+    //   );
+    // case AddressScreen.routeName:
+    //   var totalAmount =
+    //       routeSettings.arguments
+    //           as String;
+    //   return MaterialPageRoute(
+    //     settings: routeSettings,
+    //     builder:
+    //         (_) => AddressScreen(
+    //           totalAmount: totalAmount,
+    //         ),
+    //   );
+    // case OrderDetailScreen.routeName:
+    //   var order =
+    //       routeSettings.arguments
+    //           as Order;
+    //   return MaterialPageRoute(
+    //     settings: routeSettings,
+    //     builder:
+    //         (_) => OrderDetailScreen(
+    //           order: order,
+    //         ),
+    //   );
     default:
       return MaterialPageRoute(
+        settings: routeSettings,
         builder:
             (_) => const Scaffold(
               body: Center(
                 child: Text(
-                  "Screen does not exist",
+                  'Screen does not exist!',
                 ),
               ),
             ),
