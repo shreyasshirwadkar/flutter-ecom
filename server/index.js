2964ce60f044d057c335a23a16c67414c1012766
Shreyas Shirwadkar <shreyas.shirwadkar1@gmail.com>
2025-04-25 19:09:45 +0530

    Done

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 const mongoose = require("mongoose");
 const authRouter = require("./routes/auth");
 const adminRouter = require("./routes/admin");
