c970476ba47ae0969e6c700ccfbb33e0eba39386
Shreyas Shirwadkar <shreyas.shirwadkar1@gmail.com>
2025-04-25 19:11:28 +0530

    URI

--- lib/constants/global_variables.dart
+++ lib/constants/global_variables.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
-String uri = 'http://192.168.56.1:3000';
+String uri =
+    'https://flutter-ecom-7mj0.onrender.com/';
 
 class GlobalVariables {
   // COLORS
